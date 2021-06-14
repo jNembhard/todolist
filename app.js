8e853c3ad5f097b7b8ffb3ca00b73f7e07f5a489
jNembhard <nembhardjl@outlook.com>
2021-06-14 18:34:01 -0400

    String Edit

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const item2 = new Item({
 });
 
 const item3 = new Item({
-  name: "<-- Hit this to delete an item."
+  name: "<-- Click this to delete an item."
 });
 
 const defaultItems = [item1, item2, item3];
